34465c36ea23eb54aec834614ffcacebd53590c8
Roberval Filho <rslfilho@gmail.com>
2021-10-06 14:18:05 -0300

    Variaveis de ambientes

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,9 +4,9 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "rslfilho-portfolio.firebaseapp.com",
-  projectId: "rslfilho-portfolio",
-  storageBucket: "rslfilho-portfolio.appspot.com",
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
 };
